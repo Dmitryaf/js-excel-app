64f3dc7927cd91860746f68fb23939ab5dc80b3d
Dmitry <dmitrytbp@gmail.com>
2020-10-03 15:39:20 +0700

    Add resize element

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,12 @@ import {createTable} from '@/components/table/table.template';
 export class Table extends ExcelCopmonent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown', 'mousemove', 'mouseup'],
+    });
+  }
+
   toHTML() {
     return createTable();
   }
