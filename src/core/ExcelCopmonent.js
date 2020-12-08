1c8f7cffc2e6cf2c4c10c6b11d3d9661acecec2c
Dmitry <dmitrytbp@gmail.com>
2020-12-08 19:26:20 +0700

    Added emitter methods

--- src/core/ExcelCopmonent.js
+++ src/core/ExcelCopmonent.js
@@ -4,10 +4,13 @@ export class ExcelCopmonent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
 
     this.prepare();
   }
 
+  // Настраиваем компонент до init
   prepare() {}
 
   // Возвращает шаблон компонента
@@ -15,11 +18,25 @@ export class ExcelCopmonent extends DomListener {
     return '';
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
   init() {
     this.initDOMListeners();
   }
 
-  removeListener() {
+  // Удаляем компонент
+  // Чистим слушателей
+  destroy() {
     this.removeDOMListeners();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
