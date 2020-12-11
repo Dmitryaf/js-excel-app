bc1f968f5ea7a2fe2a58bfdcbf14d83dbce8a323
Dmitry <dmitrytbp@gmail.com>
2020-12-11 14:22:55 +0700

    Create createStore.js

---
+++ src/core/createStore.js
@@ -0,0 +1,22 @@
+export function createStore(rootReducer, initialState = {}) {
+  let state = rootReducer({...initialState}, {type: 'INIT'});
+  let listeners = [];
+
+  return {
+    subscribe(fn) {
+      listeners.push(fn);
+      return {
+        unsubscribe() {
+          listeners = listeners.filter((listener) => listener !== fn);
+        },
+      };
+    },
+    dispatch(action) {
+      state = rootReducer(state, action);
+      listeners = listeners.forEach((listener) => listener());
+    },
+    getStore() {
+      return state;
+    },
+  };
+}
