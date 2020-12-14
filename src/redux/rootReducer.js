7ac6df843077a625405c3b31d5f09ec8d1153546
Dmitry <dmitrytbp@gmail.com>
2020-12-14 16:17:14 +0700

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,1 +1,17 @@
-export function rootReducer(state, action) {}
+import {TABLE_RESIZE} from '@/redux/types';
+
+export function rootReducer(state, action) {
+  let colsData;
+  switch (action.type) {
+    case TABLE_RESIZE:
+      colsData = state.colState || {};
+      colsData[action.data.id] = action.data.value;
+      return {
+        ...state,
+        colState: colsData,
+      };
+
+    default:
+      return state;
+  }
+}
