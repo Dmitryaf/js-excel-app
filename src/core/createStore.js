6dd5b43fee0acfefd03f1dd76c7120f4355db9c6
Dmitry <dmitrytbp@gmail.com>
2020-12-17 17:03:09 +0700

    Update createStore.js

--- src/core/createStore.js
+++ src/core/createStore.js
@@ -16,7 +16,7 @@ export function createStore(rootReducer, initialState = {}) {
       listeners.forEach((listener) => listener(state));
     },
     getStore() {
-      return state;
+      return JSON.parse(JSON.stringify(state));
     },
   };
 }
