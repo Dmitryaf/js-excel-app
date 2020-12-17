31af33a01055544d2dd9023cafa1912ad9d8bd81
Dmitry <dmitrytbp@gmail.com>
2020-12-17 19:08:22 +0700

    Create ExcelStateComponent.js
    For controlling state of components

---
+++ src/core/ExcelStateComponent.js
@@ -0,0 +1,20 @@
+import {ExcelCopmonent} from '@core/ExcelCopmonent';
+
+export class ExcelStateComponent extends ExcelCopmonent {
+  constructor(...args) {
+    super(...args);
+  }
+
+  get template() {
+    return JSON.stringify(this.state, null, 2);
+  }
+
+  initState(initialState = {}) {
+    this.state = initialState;
+  }
+
+  setState(newState) {
+    this.state = {...this.state, ...newState};
+    this.$root.html(this.template);
+  }
+}
