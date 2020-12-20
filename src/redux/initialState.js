75673349c015dcb24c902aab2d4a07bf457157e3
Dmitry <dmitrytbp@gmail.com>
2020-12-20 11:04:08 +0700

    Update initialState.js

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,14 +1,22 @@
 import {storage} from '@core/utils';
-import {defaultStyles} from '@/constants';
+import {defaultStyles, defaultTitle} from '@/constants';
 
 const defaultState = {
   colState: {},
   rowState: {},
   dataState: {},
+  stylesState: {},
   currentText: '',
+  tableTitle: defaultTitle,
   currentStyles: defaultStyles
 };
 
+const normalize = (state) => ({
+  ...state,
+  currentText: '',
+  currentStyles: defaultStyles
+});
+
 export const initialState = storage('excel-state')
-  ? storage('excel-state')
+  ? normalize(storage('excel-state'))
   : defaultState;
